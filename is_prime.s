4839af8b5db2b5a08a561112271eab8dee8b3a74
Hilton Shumway <hillshum@gmail.com>
2017-06-30 21:46:44 +0000

    Fix bug in finding primes

--- is_prime.s
+++ is_prime.s
@@ -4,7 +4,7 @@
 
 .global is_prime
 is_prime: // r0 is the potential prime
-    push {r4, r5, r6, r7, r8, lr}
+    push {r3, r4, r5, r6, r7, r8, r9, r10, r11, lr}
 
     mov r4, r0 // Save the input
     bl sqrt
@@ -12,31 +12,32 @@ is_prime: // r0 is the potential prime
     
     mov r6, #3 // Number divided by
 
+test:
+    cmp r6, r5
+    bhi prime
+
 loop:
     mov r0, r4
     mov r1, r6
     bl mod
 
-
-test:
+after_mod:
     cmp r0, #0
     beq composite
 
     add r6, r6, #2
-    cmp r6, r5
-    bpl prime
 
-    b loop
+    b test
 
 
 
 prime: // return 1
     mov r0, #1
-    pop {r4, r5, r6, r7, r8, pc}
+    pop {r3, r4, r5, r6, r7, r8, r9, r10, r11, pc}
 
 
 composite: // return 0
     mov r0, #0
-    pop {r4, r5, r6, r7, r8, pc}
+    pop {r3, r4, r5, r6, r7, r8, r9, r10, r11, pc}
 
 
